a3b050595d962982156efeb5668567cf98f8aa21
WebMucidi <fatihcelik1907@gmail.com>
2023-05-10 12:52:18 +0300

    kitap filtreleme

--- kitaplar.js
+++ kitaplar.js
@@ -1,16 +1,21 @@
+const kaynakUrl = "https://api.collectapi.com/book/newBook";
+const baglantiAyarlari = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": "apikey 2Jl8FuVzEAZ1F8dgDw3roF:4tpY0zR9ayGurnenC9MJCy"
+  }
+};
+
+let sunucuYaniti;
+let kitaplar;
+
 async function kitapListele() {
-    const kaynakUrl = "https://api.collectapi.com/book/newBook";
-    const baglantiAyarlari = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "senin api keyin"
-      }
-    };
+
   
     try {
-      const sunucuYaniti = await fetch(kaynakUrl, baglantiAyarlari);
-      const kitaplar = await sunucuYaniti.json();
+      sunucuYaniti = await fetch(kaynakUrl, baglantiAyarlari);
+      kitaplar = await sunucuYaniti.json();
       console.log(kitaplar);
 
       kitaplar.result.forEach(kitap => {
@@ -36,6 +41,44 @@ async function kitapListele() {
     }
   }
 
+
+document.getElementById("filterButton").addEventListener("click",()=>{
+  let arananYazar=document.getElementById("txtYazar").value;
+  kitapFiltrele(arananYazar);
+});
+
+  async function kitapFiltrele(yazar){
+
+    const booksDiv = document.querySelector('.kitaplar');
+    booksDiv.innerHTML = ''; // Clear any existing book cards
+
+    try {
+      sunucuYaniti = await fetch(kaynakUrl, baglantiAyarlari);
+      kitaplar = await sunucuYaniti.json();
+      
+      const secilmisKitaplar = kitaplar.result.filter(kitap => kitap.yazar === yazar);
+      console.log(secilmisKitaplar);
+
+      kitaplar.result.forEach(kitap => {
+
+        const kitapKarti = document.createElement('div');
+        kitapKarti.classList.add('book-card');
+        document.querySelector(".kitaplar").appendChild(kitapKarti);
+
+        const kitapBasligi=document.createElement("h3");
+        kitapBasligi.innerHTML=kitap.title;
+        kitapKarti.appendChild(kitapBasligi);
+
+        const kitapKapak=document.createElement("img");
+        kitapKapak.src=kitap.image;
+        kitapKarti.appendChild(kitapKapak);
+
+      });
+    } 
+    catch (error) {
+      console.error(`Error fetching books in ${yazar}:`, error);
+    }
+  }
 
   kitapListele();
   
--- script.js
+++ script.js
@@ -1,25 +1,65 @@
+const apiUrl = "https://api.collectapi.com/book/newBook";
+const options = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": "apikey 2Jl8FuVzEAZ1F8dgDw3roF:4tpY0zR9ayGurnenC9MJCy"
+  }
+};
+
+let data;
+let response;
+
+const filterButton=document.getElementById("filterButton");
+
+filterButton.addEventListener("click",()=>{
+  
+    let arananYazar=document.getElementById("yazar").value;
+    filterByYazar(arananYazar);
+    
+}
+);
+
+
 async function getNewBook() {
-    const url = "https://api.collectapi.com/book/newBook";
-    const options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "senin api keyin"
-      }
-    };
   
     try {
-      const response = await fetch(url, options);
-      const data = await response.json();
+      response = await fetch(apiUrl, options);
+      data = await response.json();
+      console.log(data.result);
       data.result.forEach(element => {
         const title = document.createElement('h2');
         title.textContent = element.title;
         document.querySelector(".books").appendChild(title);
       });
-      console.log(data);
+      //console.log(data);
     } catch (error) {
       console.error(error);
     }
   }
+
+  async function filterByYazar(yazar) {
+
+    const booksDiv = document.querySelector('.books');
+    booksDiv.innerHTML = ''; // Clear any existing book cards
+    
+    try {
+
+
+      response = await fetch(apiUrl, options);
+      data = await response.json();
+      
+
+      const filteredBooks = data.result.filter(book => book.yazar === yazar);
+      console.log(filteredBooks);
+
+      filteredBooks.forEach(book => {
+        console.log(book.title);
+      });
+    } catch (error) {
+      console.error(`Error fetching books in ${yazar}:`, error);
+    }
+  }
+
 
   getNewBook();
